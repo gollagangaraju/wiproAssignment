7868453f17245950f1f429e9fe6ccbdf8736270d
gollagangaraju <gollagangaraju26@gmail.com>
2021-01-26 20:41:39 +0530

    UI complated with Mat table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+
+import { employees } from './data.service';
+
+
+export interface UserData {
+  name: string;
+  email: string;
+  age: string;
+  department: [];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'wiproAssignment';
+  displayedColumns: string[] = ['name', 'email', 'age', 'department'];
+  dataSource: MatTableDataSource<UserData>;
+  employeeData:any=employees
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  filterForm: FormGroup;
+  employeesData:any=employees
+  departmentList: any=[];
+
+  constructor(private formBuilder:FormBuilder) {
+    this.dataSource = new MatTableDataSource(this.employeeData);
+    console.log(this.dataSource.filteredData.length)
+  }
+
+  ngOnInit(){
+    this.filterForm=this.formBuilder.group({
+      department:[''],
+      search:['']
+    })
+    console.log(this.employeesData)
+    this.employeesData.forEach(element => {
+      element.departments.forEach(elements => {
+        let index=this.departmentList.findIndex(x=>x===elements) 
+        if(index===-1){
+          this.departmentList.push(elements)       
+        }
+      });
+    });
+    console.log(this.departmentList)
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  
+
+  reset(){
+    this.filterForm.reset()
+    this.dataSource=this.employeesData
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [ BrowserModule],
+  imports: [ 
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSortModule ,
+    MatToolbarModule,
+    MatTooltipModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
